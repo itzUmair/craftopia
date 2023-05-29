3900168df40531041dff3a51c0da8da0c96335c7
umair <muhammadumair2511@gmail.com>
2023-05-29 17:21:20 +0500

    fixed bugs

--- runserver.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import os
-
-env = "source venv/bin/activate" if os.name != "nt" else "venv\\Scripts\\activate.bat"
-subprocess.check_call(env, shell=True)
-subprocess.check_call(["uvicorn", "app:main", "--reload"])
