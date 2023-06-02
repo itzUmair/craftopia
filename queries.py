adf0a914e4fd88b9d7b99e119947d13fda069422
umair <muhammadumair2511@gmail.com>
2023-06-02 19:17:30 +0500

    fixed queries

--- queries.py
+++ queries.py
@@ -1,12 +1,12 @@
 # This file contains all the queries.
 # I have seperated this file to simplify the files
 
-getAllItemQuery = "SELECT * FROM product"
+getAllItemQuery = "SELECT product.*, AVG(reviews.rating), category.category_name, COUNT(reviews.review_id) FROM product LEFT JOIN reviews ON product.product_id = reviews.product_id LEFT JOIN category ON product.category_id = category.category_id GROUP BY product.product_id;"
 
-getItemQuery = "SELECT * FROM product WHERE product.product_id = %s"
+getItemQuery = "SELECT product.*, AVG(reviews.rating), category.category_name, COUNT(reviews.review_id) FROM product LEFT JOIN reviews ON product.product_id = reviews.product_id LEFT JOIN category ON product.category_id = category.category_id GROUP BY product.product_id; WHERE product.product_id = %s"
 
 getCustomerDetailQuery = "SELECT CONCAT(fname,' ',lname) as 'Customer Name', address, email FROM customer WHERE customer_id = %s"
 
 getAllProductOfSellerQuery = "SELECT product_name, price_per_unit, stock_in_inventory FROM product WHERE seller_id = %s"
 
-getAllProductsByCategoryQuery = "SELECT product_name, price_per_unit, stock_in_inventory FROM product WHERE category_id = %s"
+getAllProductsByCategoryQuery = "SELECT product.*, AVG(reviews.rating), category.category_name, COUNT(reviews.review_id) FROM product LEFT JOIN reviews ON product.product_id = reviews.product_id LEFT JOIN category ON product.category_id = category.category_id GROUP BY product.product_id HAVING category_id = %s"
